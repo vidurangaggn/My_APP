428b12e3ddc1cceb59e634f5115c901c28c3eb50
nipulviduranga <e19413@eng.pdn.ac.lk>
2024-03-03 16:38:20 +0530

    Start App

---
+++ lib/main.dart
@@ -0,0 +1,78 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_application_7/views/login_view.dart';
+import 'package:flutter_application_7/views/register_view.dart';
+import 'firebase_options.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) { 
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      //home: const RegisterView(),
+      home: const HomePage(),
+    ); 
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ), 
+      body: FutureBuilder(
+        future: Firebase.initializeApp(
+          options: DefaultFirebaseOptions.currentPlatform,
+        ),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.done:
+              final user = FirebaseAuth.instance.currentUser;
+              if (user?.emailVerified ?? false) {
+                print("You Are verifed!");
+              } else {
+                print("You need to verify your Email!");
+              }
+              return const Text("Done");
+            
+            default:
+              return const Text("Loading...");
+          }
+        },
+      )
+    );
+  }
+}
+
---
+++ lib/views/register_view.dart
@@ -0,0 +1,109 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_application_7/firebase_options.dart';
+
+class RegisterView extends StatefulWidget {
+  const RegisterView({super.key});
+
+  @override
+  State<RegisterView> createState() => _RegisterViewState();
+}
+
+class _RegisterViewState extends State<RegisterView> {
+  // This widget is the home page of your application. It is stateful, meaning
+  late final TextEditingController _email;
+  late final TextEditingController _password;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    _email = TextEditingController();
+    _password = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _email.dispose();
+    _password.dispose();
+    super.dispose();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+     return Scaffold(
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title:const Text('Register'),
+        ),
+        body: FutureBuilder(
+
+            future: Firebase.initializeApp(
+              options: DefaultFirebaseOptions.currentPlatform,
+             
+            ),
+          builder: (context,snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.done:
+                  return Column(
+              children: [
+                TextField(
+                  controller: _email,
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                    hintText: 'Enter your email',
+                  ),
+                ),
+                TextField(
+                  controller: _password,
+                    obscureText: true,
+                    enableSuggestions: false,
+                    autocorrect: false,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    hintText: 'Enter your password',
+                    
+                  ),
+                ),
+                Center(
+                  child: TextButton(
+                   onPressed: () async{
+                      final email = _email.text;
+                      final password = _password.text;
+                      try{
+                        await FirebaseAuth.instance.createUserWithEmailAndPassword(email: email, password: password);
+                      } on FirebaseAuthException catch (e) {
+                        if (e.code == 'weak-password') {
+                          print('The password provided is too weak.');
+                        } else if (e.code == 'email-already-in-use') {
+                          print('The account already exists for that email.');
+                        }
+                      } catch (e) {
+                        print(e);
+                      }
+                     
+                   },
+                   child :const Text('Register'),
+                ),
+                
+                ),
+              ],
+            );
+         
+              default:
+                return const Text('Loading');
+            }
+          
+          }
+        ) 
+
+    );
+  }
+}
+
+
+
